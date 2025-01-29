cca987ee1769b5ebcbdebf29f6bb57ac2c913cfb
MaybeBroken <davidsponseller123@gmail.com>
2025-01-28 17:18:11 -0700

    Add initial implementation of OpenXR examples and platform plugins

---
+++ pyopenxr_examples-main/xr_examples/hello_xr.py
@@ -0,0 +1,4 @@
+from hello_xr.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ vr-test-1/api/__init__.py
@@ -0,0 +1,3 @@
+"""
+Based on example from https://github.com/KhronosGroup/OpenXR-SDK-Source/tree/main/src/tests/hello_xr
+"""
